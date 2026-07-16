c5565af74ed85636703b2081e09ebf12401718b3
saniashinde <saniashinde04@gmail.com>
2026-07-16 16:39:07 +0530

    refactor: migrate search page to react query

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,66 +1,54 @@
 import { useSearchParams } from "react-router-dom";
-import { useState, useEffect } from "react";
 import service from "../../appwrite/config";
 import Container from "@mui/material/Container";
 import { PostCard } from "@/components";
+import { useQuery } from "@tanstack/react-query";
 
 function Search() {
     const [searchParams] = useSearchParams();  //everything after ? come in the query parameter q=React
     const query = searchParams.get("q") || "";
 
-    const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const { data: posts = [],
+        isLoading,
+        error } = useQuery({
+            queryKey: ["search", query],
+            queryFn: (() => service.searchPosts(query)),
+            enabled: !!query.trim(),
+            select: ((data) => data.documents)
 
-    useEffect(() => {
-        async function searchPosts() {
+        });
 
-            if (!query.trim()) {
-                setPosts([]);
-                setLoading(false);
-                return;
-            }
-            const res = await service.searchPosts(query);
-            console.log("Query:", query);
-            console.log("Response:", res);
-            if (res) {
-                setPosts(res.documents);
-            }
-            setLoading(false);
 
-        }
-        searchPosts();
-    }, [query]);
-
-    if (loading) {
-        return (
-            <Container>
-                <h1 className="py-10 text-center text-lg">
-                    Searching...
-                </h1>
-            </Container>
-        );
-    }
+if (isLoading) {
     return (
-        <Container className="py-10">
-
-            <h1 className="text-3xl font-bold mb-8">
-                Search results for "{query}"
+        <Container>
+            <h1 className="py-10 text-center text-lg">
+                Searching...
             </h1>
-
-            {posts.length === 0 ? (
-                <p className="text-slate-500">
-                    No posts found.
-                </p>
-            ) : (
-                <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
-                    {posts.map((post) => (
-                        <PostCard key={post.$id} {...post} />
-                    ))}
-                </div>
-            )}
-
         </Container>
-    )
+    );
 }
+return (
+    <Container className="py-10">
+
+        <h1 className="text-3xl font-bold mb-8">
+            Search results for "{query}"
+        </h1>
+
+        {posts.length === 0 ? (
+            <p className="text-slate-500">
+                No posts found.
+            </p>
+        ) : (
+            <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
+                {posts.map((post) => (
+                    <PostCard key={post.$id} {...post} />
+                ))}
+            </div>
+        )}
+
+    </Container>
+)
+};
 
 export default Search
