d47935e4f3ae3cd1d8d3bed384e7171c22cfc8c1
saniashinde <saniashinde04@gmail.com>
2026-07-16 16:57:35 +0530

    refactor:migrate edit page to react query

--- src/Pages/EditPost.jsx
+++ src/Pages/EditPost.jsx
@@ -4,25 +4,21 @@ import { Container } from "../components";
 import {PostForm} from "../components";
 import service from "../../appwrite/config";
 import { useNavigate, useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 function EditPost(){
-    const [post,setPost]=useState(null)
     const {slug}=useParams()   //used to get slug value from URL
     const navigate=useNavigate()
 
-    useEffect(()=>{
-        if(slug){
-            service.getPost(slug)
-            .then((post)=>{
-                if(post){
-                    setPost(post)
-                }
-            })
-        }
-        else{
-            navigate("/")
-        }
-    },[slug,navigate])
+    const {data:post,
+        isLoading,
+        error
+    }=useQuery({
+        queryKey:["edit-post",slug],
+        queryFn:(()=>service.getPost(slug)),
+        enabled:!!slug
+    });
+
     return post?(
         <div className="py-8">
             <Container>
