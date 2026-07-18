3e0eaffd4ba44aa33a18854fcc0c022367f81e65
saniashinde <saniashinde04@gmail.com>
2026-07-18 17:04:07 +0530

    perf: lazy load TinyMCE editor with React.lazy

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Editor } from "@tinymce/tinymce-react";
+import { lazy, Suspense } from "react";
+
+const Editor = lazy(() =>
+    import("@tinymce/tinymce-react").then((module) => ({
+        default: module.Editor,
+    }))
+);
 import { Controller } from "react-hook-form";
 
 //control passess control from this component to the one where it is called
@@ -16,41 +22,46 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
                 name={name || "content"}
                 control={control}
                 render={({ field: { onChange } }) => {
-                    return (<Editor
-                        apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
-                        initialValue={defaultValue}
-                        init={{
-                            initialValue: defaultValue,
-                            height: 500,
-                            menubar: true,
-                            plugins: [
-                                "image",
-                                "advlist",
-                                "autolink",
-                                "lists",
-                                "link",
-                                "image",
-                                "charmap",
-                                "preview",
-                                "anchor",
-                                "searchreplace",
-                                "visualblocks",
-                                "code",
-                                "fullscreen",
-                                "insertdatetime",
-                                "media",
-                                "table",
-                                "code",
-                                "help",
-                                "wordcount",
-                                "anchor",
-                            ],
-                            toolbar:
-                                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-                            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
-                        }}
-                        onEditorChange={onChange}
-                    />)
+                    return (
+                        <Suspense fallback={<div>Loading editor...</div>}>
+                            <Editor
+                                apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
+                                initialValue={defaultValue}
+                                init={{
+                                    initialValue: defaultValue,
+                                    height: 500,
+                                    menubar: true,
+                                    plugins: [
+                                        "image",
+                                        "advlist",
+                                        "autolink",
+                                        "lists",
+                                        "link",
+                                        "image",
+                                        "charmap",
+                                        "preview",
+                                        "anchor",
+                                        "searchreplace",
+                                        "visualblocks",
+                                        "code",
+                                        "fullscreen",
+                                        "insertdatetime",
+                                        "media",
+                                        "table",
+                                        "code",
+                                        "help",
+                                        "wordcount",
+                                        "anchor",
+                                    ],
+                                    toolbar:
+                                        "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help",
+                                    content_style:
+                                        "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+                                }}
+                                onEditorChange={onChange}
+                            />
+                        </Suspense>
+                    );
                 }} />
         </div>
 
